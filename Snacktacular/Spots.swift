77c49851ea48691fb4f19de2c7127243f57eb5ec
Paige Carey <peace.paigey@gmail.com>
2018-11-02 15:12:17 -0400

    9.2 complete

---
+++ Snacktacular/Spots.swift
@@ -0,0 +1,13 @@
+//
+//  Spots.swift
+//  Snacktacular
+//
+//  Created by Paige Carey on 11/2/18.
+//  Copyright © 2018 John Gallaugher. All rights reserved.
+//
+
+import Foundation
+
+class Spots {
+    var spotArray = [Spot]()
+}
